97a0425db73bbd27f384e9cc10c6d449293b2ab0
Alexander Viklund <viklu@kth.se>
2019-09-23 00:37:26 +0200

    :tractor: :boom: Basically rewrite backend

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from os import getenv
 from io import BytesIO
+from typing import Dict, Tuple
 
-from flask import Flask, request, redirect, jsonify, send_file, Response
-from werkzeug.contrib.fixers import ProxyFix
+from flask import Flask, request, redirect, jsonify, send_file, Response, abort
+from werkzeug.middleware.proxy_fix import ProxyFix
 from werkzeug.urls import url_quote
 
+from functools import wraps
+from datetime import datetime, timedelta
 from requests import get
 from magic import from_buffer
 from PIL import Image
@@ -15,103 +18,147 @@ app = Flask(__name__, static_url_path='', static_folder='build')
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 LOGIN_API_KEY = getenv('LOGIN_API_KEY')
-LOGIN_HOST = getenv('LOGIN_HOST')
-API_HOST = getenv('API_HOST')
+LOGIN_HOST = getenv('LOGIN_HOST', 'https://login.datasektionen.se')
+HODIS_HOST = getenv('HODIS_HOST', 'https://hodis.datasektionen.se')
 
-def verify_token(token):
+MISSING = s3.get('missing.svg')['Body'].read()
+
+login_cache: Dict[str, Tuple[str, datetime]] = dict()
+
+
+def verify_token(token: str):
+    if token in login_cache and login_cache[token][1] > datetime.now() - timedelta(hours=1):
+        return login_cache[token][0]
+
     payload = {'format': 'json', 'api_key': LOGIN_API_KEY}
-    response = get('{}/verify/{}'.format(LOGIN_HOST, token), params=payload)
+    response = get(f'{LOGIN_HOST}/verify/{token}', params=payload)
     if response.status_code == 200:
-        return response.json()['user']
+        user = response.json()['user']
+        login_cache[token] = (user, datetime.now())
+        return user
     else:
-        return False
-
-@app.route('/')
-def index():
-    user = verify_token(request.args.get('token'))
-    if user:
-        return app.send_static_file('index.html')
-    else:
-        return redirect('{}/login?callback={}?token='.format(LOGIN_HOST, url_quote(request.base_url)))
+        return None
 
 
-def path(user):     return '{}/{}/{}'.format(user[0], user[1], user)
-def personal(path): return 'personal_images/{}'.format(path.lower())
-def original(path): return 'original_images/{}'.format(path.lower())
+def require_login(user_param_name='user'):
+    def inner_decorator(func):
+        @wraps(func)
+        def wrapped_func(*args, **kwargs):
+            token = None
+            if request.method == 'GET':
+                token = request.args.get('token')
+            elif request.method == 'POST':
+                token = request.values.get('token')
+            user = None if token is None else verify_token(token)
 
-@app.route('/me')
-def me():
-    user = verify_token(request.args.get('token'))
-    if user:
-        return jsonify({
-            'uid': user,
-            'personal': s3.exists(personal(path(user)))
-        })
-    else:
-        return jsonify({
-            'uid': 'unknown',
-            'personal': False
-        })
+            if user is None:
+                return redirect(f'{LOGIN_HOST}/login?callback={url_quote(request.base_url)}?token=')
 
-missing = s3.get('missing.svg')['Body'].read()
+            kwargs[user_param_name] = user
+            return func(*args, **kwargs)
 
-@app.route('/user/<user>/image', methods=['GET', 'POST', 'DELETE'])
+        return wrapped_func
+
+    return inner_decorator
+
+
+# noinspection PyUnusedLocal
+@app.route('/', methods=['GET'])
+@require_login()
+def index(user):
+    # Don't give a flying flamingo about the user, just that you're logged in
+    return app.send_static_file('index.html')
+
+
+def path(user):
+    return '{}/{}/{}'.format(user[0], user[1], user)
+
+
+def personal_path(user):
+    return f'personal_images/{path(user).lower()}'
+
+
+def original_path(user):
+    return f'original_images/{path(user).lower()}'
+
+
+@app.route('/me', methods=['GET'])
+@require_login()
+def me(user):
+    return jsonify({
+        'uid': user,
+        'personal': s3.exists(personal_path(user))
+    })
+
+
+@app.route('/user/<user>/image', methods=['GET'])
 def user_image(user):
-    if request.method == 'GET':
-        if s3.exists(personal(path(user))):
-            obj = s3.get(personal(path(user)))
-            return send_file(obj['Body'], mimetype=obj['ContentType'])
-        elif s3.exists(original(path(user))):
-            obj = s3.get(original(path(user)))
-            return send_file(obj['Body'], mimetype=obj['ContentType'])
-        else:
-            return Response(missing, content_type='image/svg+xml')
-
-    elif request.method == 'POST' and verify_token(request.values.get('token')):
-        image = request.files['file']
-        mimetype = from_buffer(image.stream.read(1024), mime=True)
-        s3.put(personal(path(user)), image, mimetype)
-        return 'Personal image uploaded successfully'
-
-    elif request.method == 'DELETE' and verify_token(request.values.get('token')):
-        s3.delete(personal(path(user)))
-        return 'Personal image deleted successfully'
+    if s3.exists(personal_path(user)):
+        obj = s3.get(personal_path(user))
+        return send_file(obj['Body'], mimetype=obj['ContentType'])
+    elif s3.exists(original_path(user)):
+        obj = s3.get(original_path(user))
+        return send_file(obj['Body'], mimetype=obj['ContentType'])
     else:
-        return 'You can only edit you own picture!'
+        return Response(MISSING, content_type='image/svg+xml')
 
-@app.route('/user/<user>/image/<int:size>')
+
+@app.route('/user/<user>/image', methods=['POST'])
+@require_login('req_user')
+def edit_user_image(user, req_user):
+    if user != req_user:
+        abort(403)
+    image = request.files['file']
+    mimetype = from_buffer(image.stream.read(1024), mime=True)
+    s3.put(personal_path(user), image, mimetype)
+    return "Personal image uploaded successfully"
+
+
+@app.route('/user/<user>/image', methods=['DELETE'])
+@require_login('req_user')
+def delete_user_image(user, req_user):
+    if user != req_user:
+        abort(403)
+    s3.delete(personal_path(user))
+    return "Personal image deleted successfully"
+
+
+@app.route('/user/<user>/image/<int:size>', methods=['GET'])
 def user_image_resize(user, size):
     tmp = False
-    if s3.exists(personal(path(user))):
-        tmp = BytesIO(s3.get(personal(path(user)))['Body'].read())
-    elif s3.exists(original(path(user))):
-        tmp = BytesIO(s3.get(original(path(user)))['Body'].read())
+    if s3.exists(personal_path(user)):
+        tmp = BytesIO(s3.get(personal_path(user))['Body'].read())
+    elif s3.exists(original_path(user)):
+        tmp = BytesIO(s3.get(original_path(user))['Body'].read())
 
-    if tmp:
-        image = Image.open(tmp)
-        image.thumbnail((size, size), Image.ANTIALIAS)
-        tmp = BytesIO()
-        image.save(tmp, 'JPEG')
-        tmp.seek(0)
-        return Response(tmp, content_type='image/jpeg')
-    else:
-        return missing
+    if not tmp:
+        tmp = BytesIO(MISSING)
+
+    image = Image.open(tmp)
+    image.thumbnail((size, size), Image.ANTIALIAS)
+    tmp = BytesIO()
+    image.save(tmp, 'JPEG')
+    tmp.seek(0)
+    return Response(tmp, content_type='image/jpeg')
+
 
 # Redirects to the old API
-@app.route('/users/<query>')
-def users(query):
-    resp = redirect(API_HOST + '/users/' + query)
+@app.route('/users/<query>', methods=['GET'])
+def find_users(query):
+    resp = redirect(HODIS_HOST + '/users/' + query)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
-@app.route('/ugkthid/<id>')
-def ugkthid(id):
-    resp = redirect(API_HOST + '/ugkthid/' + id)
+
+@app.route('/ugkthid/<ugid>', methods=['GET'])
+def ugkthid(ugid):
+    resp = redirect(HODIS_HOST + '/ugkthid/' + ugid)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
+
 
 @app.route('/user/<user>', methods=['GET', 'POST'])
-def user(user):
-    resp = redirect(API_HOST + '/uid/' + user)
+def user_info(user):
+    resp = redirect(HODIS_HOST + '/uid/' + user)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
