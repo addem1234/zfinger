1f721c521e1c7a5a97ebbc922ebff2a5088d459d
elmaxe <axel.elmarsson@gmail.com>
2022-03-27 12:30:28 +0200

    Rutinarbete
    Co-authored-by: Douglas Fischer <anakin_skryter@hotmail.se>

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@
  * In this file, we create a React component
  * which incorporates components provided by Material-UI.
  */
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import Methone, { Header } from "methone"
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 
 import Button from 'material-ui/Button'
 import TextField from 'material-ui/TextField'
-
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
-import { cyan } from 'material-ui/colors'
-
 import Upload from './Upload'
 import UserCard from './UserCard'
-
-const muiTheme = createMuiTheme({
-  palette: {
-    primary: cyan
-  }
-})
 
 class App extends Component {
   constructor(props, context) {
@@ -29,86 +21,107 @@ class App extends Component {
       token: window.location.search.substr(1).split('=')[1],
       results: [],
       resultLimit: 10,
-      open: false
+      open: false,
+      hasAccess: false,
     }
 
     fetch('/me')
-        .then(res => res.json())
-        .then(res => this.setState(res))
-        .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(res => {
+        this.setState(res)
+        fetch(`https://pls.datasektionen.se/api/user/${res.uid}/zfinger`)
+        .then(r => r.json())
+        .then(r => {
+          if (r.contains("search")) this.setState({ hasAccess: true })
+        })
+      })
+      .catch(err => console.log(err))
   }
 
   doSearch = e => {
-      e.preventDefault()
-      if(e.target.value.length > 2)
-        fetch(`/users/${e.target.value}`)
-            .then(res => res.json())
-            .then(res => {
-              if(res.error) return new Error(res.error)
-              else return res
-            })
-            .then(results => this.setState({results: results || [], resultLimit: 10}))
-            .catch(err => console.error(err))
+    e.preventDefault()
+    if (e.target.value.length > 2)
+      fetch(`/users/${e.target.value}`)
+        .then(res => res.json())
+        .then(res => {
+          if (res.error) return new Error(res.error)
+          else return res
+        })
+        .then(results => this.setState({ results: results || [], resultLimit: 10 }))
+        .catch(err => console.error(err))
   }
 
-  uploadClose = () => this.setState({open: false})
+  uploadClose = () => this.setState({ open: false })
 
   render() {
-    const { results, uid, resultLimit, token, open, personal } = this.state
+    const { results, uid, resultLimit, token, open, personal, hasAccess } = this.state
 
     return (
-      <MuiThemeProvider theme={muiTheme}>
-        <div style={{marginTop: '50px'}}>
-          <header>
-            <div className='header-inner'>
-              <div className='row'>
-                <div className='header-left col-md-2'></div>
-                <div className='col-md-8'>
-                  <h2>zfinger</h2>
-                </div>
-                <div className='header-right col-md-2'>
-                  { uid ? <Button
-                    className='primary-action'
-                    onClick={() => this.setState({open: true})}>
-                    My Face
-                  </Button> : false }
-                </div>
-              </div>
-            </div>
-          </header>
-          <div id='content'>
-            <Upload
-              open={open}
-              uid={uid}
-              personal={personal}
-              token={token}
-              uploadClose={this.uploadClose} />
+      // <MuiThemeProvider theme={muiTheme}>
+      <div style={{ marginTop: '50px' }}>
+        <Methone config={{
+          system_name: 'zfinger',
+          color_scheme: 'cyan',
+          links: [
+            <Link to="/">Sök</Link>,
+            <Link to="/my-face">My face</Link>,
+          ]
+        }}
+        />
+        <Header title="zfinger" />
+        <div id='content'>
+          <Routes>
+            <Route path="/" element={
+              !hasAccess ?
+              <Navigate replace to="/my-face" />
+              :
+              <>
+                <TextField
+                  autoFocus
+                  fullWidth={true}
+                  placeholder='Search'
+                  inputProps={{ style: { boxShadow: 'none', border: 0 } }}
+                  onChange={this.doSearch} />
 
-            <TextField
-              autoFocus
-              fullWidth={true}
-              placeholder='Search'
-              inputProps={{style: {boxShadow: 'none', border: 0}}}
-              onChange={this.doSearch} />
+                {
+                  results
+                    .filter((_, i) => i < resultLimit)
+                    .map(result => <UserCard key={result.uid} {...result} />)
+                }
 
-            {
-              results
-                .filter((_, i) => i < resultLimit)
-                .map(result => <UserCard key={result.uid} {...result} />)
-            }
-
-            {
-              results.length < resultLimit ? false :
-              <Button style={{margin: '0.5%'}}
+                {
+                  results.length < resultLimit ? false :
+                    <Button style={{ margin: '0.5%' }}
                       fullWidth={true}
-                      onClick={() => this.setState({resultLimit: resultLimit + 10})}>
-                Show more
-              </Button>
+                      onClick={() => this.setState({ resultLimit: resultLimit + 10 })}>
+                      Show more
+                    </Button>
+                }
+              </>
             }
-
-          </div>
+            />
+            <Route
+              path="/my-face"
+              element={
+                <>
+                  <Button
+                    onClick={() => this.setState({ open: true })}
+                  >
+                    Klicka här för att hantera din profil
+                  </Button>
+                  <Upload
+                    open={open}
+                    uid={uid}
+                    personal={personal}
+                    token={token}
+                    uploadClose={this.uploadClose}
+                  />
+                </>
+              }
+            />
+          </Routes>
         </div>
-      </MuiThemeProvider>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Methone from 'methone'
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from "react-router-dom";
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
+import { cyan } from 'material-ui/colors'
 
-const appWrapper = (<div>
-  <Methone config={{ system_name: 'zfinger', color_scheme: 'cyan', links: []}} />
-  <App />
-</div>)
 
-ReactDOM.render(appWrapper, document.getElementById('root'));
+const muiTheme = createMuiTheme({
+  palette: {
+    primary: cyan
+  }
+})
+
+ReactDOM.render(
+  <MuiThemeProvider theme={muiTheme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </MuiThemeProvider>
+  , document.getElementById('root'));
 registerServiceWorker();
